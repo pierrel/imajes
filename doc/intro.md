6362300cfd3f98e1bc8e4905399f5da001839061
Pierre Larochelle <pierrebombay@gmail.com>
2013-01-16 08:02:25 -0800

    Initial lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to imajes
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/imajes/core_test.clj
@@ -0,0 +1,7 @@
+(ns imajes.core-test
+  (:use clojure.test
+        imajes.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
